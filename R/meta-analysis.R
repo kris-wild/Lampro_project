a437c79291e7f7ac274c49148acc5f3c64aa999f
p-pottier <p.pottier@unsw.edu.au>
2023-01-12 15:15:20 +1100

    Corrected two typos in the data. Updated all analyses and saved models. Calculated Var_ARR differently when the same data was used to calculate multiple effect sizes + added a VCV matrix of correlated errors for dependent data. Also removed unncessary code and made sure everything is running properly.

--- R/meta-analysis.R
+++ R/meta-analysis.R
@@ -7,14 +7,26 @@ pacman::p_load(devtools, dplyr, metafor, patchwork, R.rsp, emmeans, ggdist, rotl
 
 ## data
 # bring in data and arrange for analysis
-data <- read.csv("./Final.Analysis.Data/Final_Meta_results_extracted_data.csv")
-data$obs <- 1:nrow(data)
+data <- read.csv("./Final.Analysis.Data/Data_matching_Pottier_et_al.csv")
 
 ### ### ### 
 ### Meta-analysis with acclimation response ratio #### 
 ### ### ### 
-data <- data %>%  mutate(ARR=((trt_mean-c_mean)/(trt_temp-c_temp)),
-                         Var_ARR = ((1/(trt_temp-c_temp))^2*(c_sd^2/c_n+trt_sd^2/trt_n)))
+
+data_cohort <- group_by(data,cohort_ID) %>% summarise(n_cohort=n()) # Identify repeated observations on the same individuals
+data<- left_join(data, data_cohort) # Join with main dataset
+
+
+data <- data %>%  mutate(ARR = ((trt_mean-c_mean)/(trt_temp-c_temp)), # Calculate ARR
+                   
+                   Var_ARR = ifelse(n_cohort=="1", # If independent, use this formula
+                                     ((1/(trt_temp - c_temp))^2*(c_sd^2/c_n+trt_sd^2/trt_n)), # use this formula 
+                                       # If non-independent, use this formula
+                                            ((1/(trt_temp - c_temp))^2*((c_sd^2 + trt_sd^2 - 2*0.5*c_sd*trt_sd)/(c_n + trt_n)))), 
+                   
+                   precision =(1/sqrt(Var_ARR))) # Calculate precision (1/SE)
+
+
 # acclimation response ratio figure
 ggplot(data) + 
   geom_vline(xintercept=0, alpha=0.5, linetype=2)+
@@ -36,9 +48,11 @@ plot(phylo_tree)
 phylo_tree$tip.label<-gsub("_", " ", phylo_tree$tip.label) # remove underscores in species names
 phylo_matrix<-vcv(phylo_tree, cor=T) # generate phylogenetic vcv matrix
 
-## Create VCV matrix for sampling errors, but V is non-positive definite....so that's an issue. We'll use nearpd for now to correct for this as there don't seem to be any major issues in the data, and I dount this will make any real differences
-  data$cluster = with(data, interaction(study_ID, s_cor))
-  V_mat <- metafor::vcalc(vi = Var_ARR, cluster = cluster, nearpd = TRUE, data = data)
+## Create VCV matrix for sampling errors because the same data was used to calculate multiple effect sizes (stepwise comparisons; shared_trt_ID)
+names(data)<-make.names(names(data))
+data<-as.data.frame(data)
+VCV_ARR <- metaAidR::make_VCV_matrix(data, V = "Var_ARR", cluster = "shared_trt_ID", obs = "obs_ID")
+  
 
 ###################################################  
 ##### Phylo, spp, observation - intercept mods
@@ -47,14 +61,14 @@ phylo_matrix<-vcv(phylo_tree, cor=T) # generate phylogenetic vcv matrix
 data$spp <- data$genus_species
 
 model_all_rand<-rma.mv(ARR~1, 
-                       V=Var_ARR,# Consider using the VCV matrix of ARR with correlated errors
+                       V=VCV_ARR,# Consider using the VCV matrix of ARR with correlated errors
                        method="REML",
                        test="t",
                        dfs="contain",
                        random=list(~1|study_ID, 
                                    ~1|genus_species, #species name with covaiance matrix
                                    ~1|spp, # species name
-                                   ~1|obs),# Consider related_temps / related_individuals
+                                   ~1|obs_ID),# Consider related_temps / related_individuals
                        R=list(genus_species=phylo_matrix),
                        data=data)
 
@@ -68,13 +82,13 @@ saveRDS(model_all_rand, "./Final.Models/Meta_analysis_models/Meta_mods_accl_rati
 
 # 2) model removing species name as random variable but keeping phylo matrix
 model_all_no_spp<-rma.mv(ARR~1, 
-                       V=Var_ARR,# Consider using the VCV matrix of ARR with correlated errors
+                       V=VCV_ARR,# Consider using the VCV matrix of ARR with correlated errors
                        method="REML",
                        test="t",
                        dfs="contain",
                        random=list(~1|study_ID, 
                                    ~1|genus_species,
-                                   ~1|obs),# Consider related_temps / related_individuals
+                                   ~1|obs_ID),# Consider related_temps / related_individuals
                        R=list(genus_species=phylo_matrix),
                        data=data)
 # check if spp drives varience
@@ -86,49 +100,31 @@ saveRDS(model_all_no_spp, "./Final.Models/Meta_analysis_models/Meta_mods_accl_ra
 
 # 3) Drop phylogeny and just check if species is important. Basically the same so keep species in.
 model_all_rand_spp<-rma.mv(ARR~1, 
-                       V=Var_ARR,# Consider using the VCV matrix of ARR with correlated errors
+                       V=VCV_ARR,# Consider using the VCV matrix of ARR with correlated errors
                        method="REML",
                        test="t",
                        dfs="contain",
                        random=list(~1|study_ID, 
                                    ~1|genus_species,
-                                   ~1|obs),# Consider related_temps / related_individuals
+                                   ~1|obs_ID),# Consider related_temps / related_individuals
                        data=data)
 summary(model_all_rand_spp)     
 i2_ml(model_all_rand_spp) 
-
 
 
 #############################################
 # Fig2A) Intercept model: **without phylogeny
 #############################################
 int_model<-rma.mv(ARR~1, 
-                  V=Var_ARR,
+                  V=VCV_ARR,
                   method="REML",
                   test="t",
                   dfs="contain",
                   random=list(~1|study_ID, 
                               ~1|genus_species,
-                              ~1|obs),# Consider related_temps / related_individuals
+                              ~1|obs_ID),
                   data=data)
 
-# Explore incorperating V matrix to deal with correlated sampling errors. Note that this matrix is non-positive definite, so matrix is bent to make PD. Interpret this caustiously. 
-int_model_Vmat<-rma.mv(ARR~1, 
-                  V=V_mat,
-                  method="REML",
-                  test="t",
-                  dfs="contain",
-                  random=list(~1|study_ID, 
-                              ~1|genus_species,
-                              ~1|obs),# Consider related_temps / related_individuals
-                  data=data)
-
-# Check which model is better supported
-AIC(int_model)   # Clear winner
-AIC(int_model_Vmat)
-
-# Model with just sampling variance is probably best to go with, but we can check using RVE if we see any differences ## Lets use RVE to account for non-independence
-int_model_RVE <- robust(int_model, cluster = data$cluster)
 
 saveRDS(int_model, "./Final.Models/Meta_analysis_models/Meta_mods_accl_ratio/meta.acc.no.phylo.rds")
 # model checks Intercept model
@@ -140,12 +136,13 @@ predict(int_model) # Prediction intervals
 # Fig2A): Trait differences mod
 ###############################
 model_trait<- rma.mv(ARR~trait-1, 
-                     V=Var_ARR,                     method="REML",
+                     V=VCV_ARR,
+                     method="REML",
                      test="t",
                      dfs="contain",
                      random=list(~1|study_ID,
                                  ~1|genus_species,
-                                 ~1|obs),# Consider related_temps / related_individuals
+                                 ~1|obs_ID),
                      data=data)
 
 ## Lets use RVE to account for non-independence
@@ -163,21 +160,18 @@ I2_model_trait
 #########################
 data$age <- factor(data$age, levels=c("adult", "juvenile", "hatchling"))
 model_age<- rma.mv(ARR~age-1, 
-                   V=Var_ARR,
+                   V=VCV_ARR,
                    method="REML",
                    test="t",
                    dfs="contain",
                    random=list(~1|study_ID, 
                                ~1|genus_species,
-                               ~1|obs),# Consider related_temps / related_individuals
+                               ~1|obs_ID),
                    data=data)
-
-## Lets use RVE to account for non-independence
-  model_age_RVE <- robust(model_age, cluster = data$cluster)
 
 saveRDS(model_age, "./Final.Models/Meta_analysis_models/Meta_mods_accl_ratio/meta.acc.age.rds")
 summary(model_age)
-Lifestage_plot <- orchard_plot(model_age, group = "study_ID", mod="age", xlab="ARR", data = data, legend.pos = "none") + 
+Lifestage_plot <- orchard_plot(model_age, group = "study_ID", mod="age", xlab="ARR", data = data, legend.pos = "none", trunk.size=6) + 
   scale_fill_manual(values = c("deepskyblue2","firebrick", "darkgoldenrod1"))+ 
   scale_color_manual(values = c("deepskyblue2","firebrick", "darkgoldenrod1"))+
   theme_classic() + 
@@ -190,20 +184,18 @@ i2_ml(model_age)
 # Fig2C) Temperate vs Tropical mod
 ####################
 model_zone<- rma.mv(ARR~zone-1, 
-                    V=Var_ARR,
+                    V=VCV_ARR,
                     method="REML",
                     test="t",
-                    dfs="contain",random=list(~1|study_ID, 
-                                              ~1|genus_species,
-                                              ~1|obs),# Consider related_temps / related_individuals
+                    dfs="contain",
+                    random=list(~1|study_ID, 
+                                ~1|genus_species,
+                                ~1|obs_ID),
                     data=data)
-
-## Lets use RVE to account for non-independence
-  model_zone_RVE <- robust(model_zone, cluster = data$cluster)
 
 saveRDS(model_zone, "./Final.Models/Meta_analysis_models/Meta_mods_accl_ratio/meta.acc.zone.rds")
 summary(model_zone)
-LatLong_plot <-orchard_plot(model_zone, group = "study_ID", mod="zone", xlab="ARR", data = data, legend.pos = "none") +
+LatLong_plot <-orchard_plot(model_zone, group = "study_ID", mod="zone", xlab="ARR", data = data, legend.pos = "none", trunk.size=6) +
   scale_fill_manual(values = c("deepskyblue2","firebrick"))+ 
   scale_color_manual(values = c("deepskyblue2","firebrick"))+
   theme_classic() + 
@@ -215,17 +207,14 @@ i2_ml(model_zone)
 ## Fig2D) major taxonomic group mod
 ####################
 model_taxon<- rma.mv(ARR~class-1, 
-                     V=Var_ARR,# Consider using the VCV matrix of ARR with correlated errors
+                     V=VCV_ARR,# Consider using the VCV matrix of ARR with correlated errors
                      method="REML",
                      test="t",
                      dfs="contain",
                      random=list(~1|study_ID, 
                                  ~1|genus_species,
-                                 ~1|obs),# Consider related_temps / related_individuals
+                                 ~1|obs_ID),
                      data=data)
-
-## Lets use RVE to account for non-independence....something wrong with this RV Estimation. Must be a bug in metafor
-  model_taxon_RVE <- robust(model_taxon, cluster = data$cluster)
 
 saveRDS(model_taxon, "./Final.Models/Meta_analysis_models/Meta_mods_accl_ratio/meta.acc.taxon.rds")
 summary(model_taxon)
@@ -240,13 +229,19 @@ orchard_plot(model_taxon, group = "study_ID", mod="class", xlab="ARR", data = da
 i2_ml(model_taxon)
 # dropping tuatara for figure purposes
 Taxon_fig_data <- data %>% filter(class != "tuatara")
-fig_model_taxon <- rma.mv(ARR~class-1,  V=Var_ARR,method="REML",test="t",dfs="contain",
+VCV_ARRt <- metaAidR::make_VCV_matrix(Taxon_fig_data, V = "Var_ARR", cluster = "shared_trt_ID", obs = "obs_ID")
+fig_model_taxon <- rma.mv(ARR~class-1,  
+                          V=VCV_ARRt,
+                          method="REML",
+                          test="t",
+                          dfs="contain",
                      random=list(~1|study_ID, 
                                  ~1|genus_species,
-                                 ~1|obs),# Consider related_temps / related_individuals
+                                 ~1|obs_ID),
                      data=Taxon_fig_data)
+
 Taxon_plot <- orchard_plot(fig_model_taxon, group = "study_ID", mod="class", xlab="ARR", 
-                           data = Taxon_fig_data, legend.pos = "bottom.right") +  
+                           data = Taxon_fig_data, legend.pos = "bottom.right", trunk.size=6) +  
   scale_fill_manual(values = c("cornsilk4", "darkgoldenrod1","darkgreen"))+ 
   scale_color_manual(values = c("cornsilk4", "darkgoldenrod1","darkgreen"))+
   theme(axis.text.y = element_text(angle = 45),
@@ -259,9 +254,28 @@ Taxon_plot <- orchard_plot(fig_model_taxon, group = "study_ID", mod="class", xla
 ####################
 # Mod bias Check
 ####################
-model.bias1 <- rma.mv(ARR ~ sqrt(vi), random = list(~1|study_ID, ~1| species, ~1|obs), V = vi, data = data)
+
+model.bias1   <- rma.mv(ARR~ sqrt(Var_ARR),  # Standard error
+                          V=VCV_ARR,
+                          method="REML",
+                          test="t",
+                          dfs="contain",
+                          random=list(~1|study_ID, 
+                                      ~1|genus_species,
+                                      ~1|obs_ID),
+                          data=data)
+
 summary(model.bias1) 
-model.bias2 <- rma.mv(ARR ~ vi, random = list(~1|study_ID, ~1| species, ~1|obs), V = vi, data = data)
+
+model.bias2   <- rma.mv(ARR~ Var_ARR,  # Variance
+                        V=VCV_ARR,
+                        method="REML",
+                        test="t",
+                        dfs="contain",
+                        random=list(~1|study_ID, 
+                                    ~1|genus_species,
+                                    ~1|obs_ID),
+                        data=data)
 summary(model.bias2)
 
 # Funnel plot
@@ -283,7 +297,7 @@ p1p2 <- submerge(p1, p2)
 
 # Overall figure
 Overall <- orchard_plot(p1p2, group = "study_ID", xlab = "ARR", data = data, 
-                        legend.pos = "none") +
+                        legend.pos = "none", trunk.size=6) +
   scale_x_discrete(labels = c('Tpref','CTmax','Overall'))+
   scale_fill_manual(values = c("deepskyblue2", "firebrick","grey20"))+ 
   scale_color_manual(values = c("deepskyblue2", "firebrick","grey20"))+
@@ -294,7 +308,14 @@ Overall <- orchard_plot(p1p2, group = "study_ID", xlab = "ARR", data = data,
 
 # Final figure  
 # left col
-left_col <- plot_grid(Overall,Lifestage_plot,LatLong_plot, labels = c('A', 'B', 'C'), label_size = 12, ncol = 1, align = 'hv', axis = 'l')
+left_col <- plot_grid(Overall,
+                      Lifestage_plot,
+                      LatLong_plot, 
+                      labels = c('A', 'B', 'C'), 
+                      label_size = 12, 
+                      ncol = 1, 
+                      align = 'hv', 
+                      axis = 'l')
 right_col <- plot_grid(Taxon_plot, labels = c('D'), label_size = 12)
 
 Fig1 <- plot_grid(left_col, right_col)  
